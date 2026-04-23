b1970237ee7dd94e903ae74b60ed2c9494bb5e07
h3658918 <u3658918@connect.hku.hk>
2026-04-23 23:57:16 +0800

    Update ai.h

--- 2113/ai.h
+++ 2113/ai.h
@@ -5,20 +5,11 @@
 #include "board.h"
 #include "deck.h"
 
-class AIPlayer : public Player {
-private:
-    int difficultyLevel; // 1 for easy, 2 for hard
-
+class AIController {
 public:
-    // desc: setup ai player
-    // input: name and difficulty level
-    // output: none
-    AIPlayer(std::string name, int difficultyLevel);
-
-    // desc: ai logic for its turn
-    // input: board reference, deck reference
-    // output: none
-    void playTurn(Board& board, Deck& deck);
+  
+    static void playTurnEasy(Player& aiPlayer, Board& board, Deck& deck);
+    static void playTurnHard(Player& aiPlayer, Board& board, Deck& deck);
 };
 
 #endif
