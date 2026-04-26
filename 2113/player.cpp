2d11c1d1496c50f2693585e2b8127c6115f96724
Cindy123t <u3628997@connect.hku.hk>
2026-04-26 23:41:26 +0800

    Update player.cpp

--- 2113/player.cpp
+++ 2113/player.cpp
@@ -1,9 +1,11 @@
 #include "player.h"
 #include <iostream>
 
+// Constructor: initialize player with empty hand
 Player::Player(std::string playerName) 
     : name(playerName), handHead(nullptr), iceBroken(false) {}
 
+// Destructor: free all tiles in hand
 Player::~Player() {
     Node* current = handHead;
     while (current != nullptr) {
@@ -15,6 +17,7 @@ Player::~Player() {
     handHead = nullptr;
 }
 
+// Add a tile to player's hand (insert at head of linked list)
 void Player::addTile(Tile* tile) {
     Node* newNode = new Node;
     newNode->tile = tile;
@@ -22,6 +25,8 @@ void Player::addTile(Tile* tile) {
     handHead = newNode;
 }
 
+// Remove a tile by number and color (remove first match found)
+// Return true if found and removed, false otherwise
 bool Player::removeTile(int number, Color color) {
     Node* prev = nullptr;
     Node* curr = handHead;
@@ -43,6 +48,7 @@ bool Player::removeTile(int number, Color color) {
     return false;
 }
 
+// Calculate total value of all tiles in hand
 int Player::getHandValue() {
     int total = 0;
     Node* current = handHead;
@@ -53,18 +59,22 @@ int Player::getHandValue() {
     return total;
 }
 
+// Check if player has broken the ice (score >= 30)
 bool Player::isIceBroken() {
     return iceBroken;
 }
 
+// Set ice broken status
 void Player::setIce(bool status) {
     iceBroken = status;
 }
 
+// Get head pointer of hand linked list
 Node* Player::getHand() {
     return handHead;
 }
 
+// Get player name
 std::string Player::getName() {
     return name;
 }
