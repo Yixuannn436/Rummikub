649f07f52f618fb7c14ad22b705f3283a9199b55
Shirasagi666 <wdfxx1016@gmail.com>
2026-05-02 22:35:03 +0800

    Change member functions to static in ui.h

--- 2113/ui.h
+++ 2113/ui.h
@@ -8,22 +8,22 @@ public:
     // desc: print a welcome message and the game title to the console
     // input: none
     // output: none
-    void printWelcome();
+    static void printWelcome();
 
     // desc: prompt the user for an integer input and validate it within a range
     // input: min (minimum allowed value), max (maximum allowed value)
     // output: the validated integer
-    int getValidInt(int min, int max);
+    static int getValidInt(int min, int max);
 
     // desc: display a single game tile with its specific color and number
     // input: t (the tile object to be displayed)
     // output: none
-    void displayTile(const Tile& t);
+    static void displayTile(const Tile& t);
 
     // desc: clear the terminal screen using ANSI escape sequences
     // input: none
     // output: none
-    void clearScreen();
+    static void clearScreen();
 };
 
 #endif
